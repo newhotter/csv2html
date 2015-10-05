f954cbe79af89f6760d09454f518c2e7b9852f97
newhotter <10512076@qq.com>
2015-10-05 10:27:41 +0800

    Update csv2html.py

--- csv2html/csv2html.py
+++ csv2html/csv2html.py
@@ -64,6 +64,8 @@ class TableGenHtml(TableGen):
             self.html = html.HTML('html')
             # self.html.text('<!DOCTYPE html>', escape=False)
             self.html.head.title(title)
+            self.html.head('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />')
+
             self.table = self.html.body.table(border='1')
         else:
             self.html = html.HTML()
